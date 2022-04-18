d9b29f1b99cf352bbf733e3e261a074addd14ff5
jay <jayyehice@gmail.com>
2022-04-18 21:33:04 +0800

    add gotop

---
+++ js/gotop.js
@@ -0,0 +1,17 @@
+
+let gotop = document.getElementById("gotop");
+
+gotop.addEventListener("click", function(){
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
+
+
+document.addEventListener('scroll', function(e) {
+
+    if(window.scrollY >= animation_high){
+        
+    }
+    
+    
+});
