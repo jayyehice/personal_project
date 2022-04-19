466aadb78f217fb1b2749185a5067f3755b9d6ee
jay <jayyehice@gmail.com>
2022-04-19 12:06:29 +0800

    update gotop & footer icon color

--- js/gotop.js
+++ js/gotop.js
@@ -2,16 +2,35 @@
 let gotop = document.getElementById("gotop");
 
 gotop.addEventListener("click", function(){
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+    //document.body.scrollTop = 0; // For Safari
+    //document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+    let current_high = window.scrollY;
+    let hight_cut = current_high / 100;
+
+    var interval_id = setInterval(function(){
+
+        if (current_high <= hight_cut){
+            clearInterval(interval_id);
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }else{
+            current_high -= hight_cut;
+            document.body.scrollTop = current_high;
+            document.documentElement.scrollTop = current_high;
+        }
+        
+      }, 2);
+
 });
 
 
 document.addEventListener('scroll', function(e) {
 
-    if(window.scrollY >= animation_high){
-        
+    if(window.scrollY >= 1000){
+        gotop.style.display="block";
+    }else{
+        gotop.style.display="none";
     }
-    
-    
+
 });
