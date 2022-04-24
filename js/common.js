3070c0bd335be78c78177d970d7dafa606e9a7d9
jay <jayyehice@gmail.com>
2022-04-25 00:23:18 +0800

    RWD 540 header & footer

---
+++ js/common.js
@@ -0,0 +1,14 @@
+
+let header_ham = document.getElementById("header_ham");
+let header_ul = document.getElementById("header_ul");
+
+header_ham.addEventListener("click", e => {
+    // console.log(header_ul.style.height);
+    if(header_ul.style.height === '205px'){
+        header_ul.removeAttribute("style");
+        
+    }else{
+        header_ul.style.height = '205px';
+        
+    }
+});
